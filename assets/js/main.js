15a6106b351a3ffcf31b22d574180e958a403c8c
Lorenzo Bernini <templarec@gmail.com>
2021-03-01 17:58:53 +0100

    Esercizio completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,22 @@ etaPass = parseInt(etaPass);
 km = parseFloat(km);
 console.log(etaPass, km);
 var prezzoKm = 0.21;
+var scontoApp = null;
 prezzoKm = prezzoKm * km;
 if (etaPass < 18) {
     prezzoKm = prezzoKm * 0.8;
+    scontoApp = "20%";
 } else if (etaPass > 64) {
     prezzoKm = prezzoKm * 0.6;
+    scontoApp = "40%";
+}
+var output = document.getElementById("output");
+output.innerHTML = "La tua età: " + etaPass + "<br>";
+output.innerHTML += "Chilometri da percorrere: " + km + " km<br>";
+output.innerHTML += "Prezzo del biglietto: " + prezzoKm + " € <br>";
+if (scontoApp != null)
+{
+    output.innerText += "Sconto applicato: " + scontoApp;
 }
 
 console.log(prezzoKm, "€");
